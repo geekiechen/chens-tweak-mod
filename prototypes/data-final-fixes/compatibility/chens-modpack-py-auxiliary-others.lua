90ff913115f2ef4b4dd59db72633209f340bab82
GeekieChen <yihao9332@outlook.com>
2026-01-14 14:18:00 +0800

    Updated version to 0.20.11, please check changelog.txt for details.

--- prototypes/data-final-fixes/compatibility/chens-modpack-py-auxiliary-others.lua
+++ prototypes/data-final-fixes/compatibility/chens-modpack-py-auxiliary-others.lua
@@ -111,48 +111,6 @@ if mods["pycoalprocessing"] then
         end
     end
 
-    -- 修复 hidden-mining-fluid-autounlock 的问题
-    if data.raw.technology["hidden-mining-fluid-autounlock"] then
-        for i = #data.raw.technology["hidden-mining-fluid-autounlock"].effects, 1, -1 do
-            local effect = data.raw.technology["hidden-mining-fluid-autounlock"]
-                               .effects[i]
-            if effect.type == "unlock-recipe" and effect.type ==
-                "mining-with-fluid" then
-                table.remove(
-                    data.raw.technology["hidden-mining-fluid-autounlock"]
-                        .effects, i)
-                break
-            end
-        end
-
-        if next(data.raw.technology["hidden-mining-fluid-autounlock"].effects or {}) ==
-            nil then
-            for _, tech in pairs(data.raw.technology) do
-                if tech.prerequisites and
-                    table.contains(tech.prerequisites, "hidden-mining-fluid-autounlock") then
-                    for i, prerequisite in ipairs(tech.prerequisites) do
-                        if prerequisite == "hidden-mining-fluid-autounlock" then
-                            table.remove(tech.prerequisites, i)
-                            break
-                        end
-                    end
-
-                    for _, prerequisite in ipairs(
-                                               data.raw.technology["hidden-mining-fluid-autounlock"]
-                                                   .prerequisites or {}) do
-                        if not table.contains(tech.prerequisites, prerequisite) then
-                            if data.raw.technology[prerequisite] then
-                                table.insert(tech.prerequisites, prerequisite)
-                            end
-                        end
-                    end
-                end
-            end
-
-            data.raw.technology["hidden-mining-fluid-autounlock"] = nil
-        end
-    end
-
     -- 修复配方的问题
     -- 修复 burner-inserter 的问题
     if data.raw.recipe["burner-inserter"] then
